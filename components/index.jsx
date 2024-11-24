d2b292a38c02ee993e19a61331a8a7c26f1be86f
jose ricardo kremer polesi <josericardo.kremer@gmail.com>
2024-11-23 21:45:32 -0300

    feat: add ToastError component export to index

--- components/index.jsx
+++ components/index.jsx
@@ -2,5 +2,6 @@ export * from "./Prompt/index.jsx";
 export * from "./SystemButton/index.jsx";
 export * from "./TitleDetails/index.jsx";
 export * from "./TitlesList/index.jsx";
+export * from "./ToastError/index.jsx";
 export * from "./WatchListMovies/index.jsx";
 export * from "./WatchListSeries/index.jsx";
